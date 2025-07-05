7e5958daf9ffefedc25a04de5be8cb2c55957fcf
Mosniff <joe.ismail.hill@gmail.com>
2025-07-05 18:43:58 +0100

    refactor tests

--- api/spec/models/general_spec.rb
+++ api/spec/models/general_spec.rb
@@ -6,43 +6,4 @@ RSpec.describe General, type: :model do
   it 'initializes correctly' do
     expect(general).to be_valid
   end
-
-  # Build a hash for quick lookup: { "general_1" => "General One", ... }
-  general_templates = {}
-  General.templates.each do |key, attrs|
-    general_templates[key.to_s] = attrs['name']
-  end
-
-  Story.templates.each_key do |template_name|
-    it "assigns generals to kingdoms correctly for template '#{template_name}'", :slow do
-      game = create(:game)
-      world = game.world
-      expect { world.select_story(template_name) }.not_to raise_error
-
-      scenario_template_key = Story.get_scenario_template_key(Story.templates[template_name])
-      scenario_template = Scenario.templates[scenario_template_key]
-
-      (scenario_template['kingdoms'] || []).each do |kingdom_attrs|
-        kingdom = world.kingdoms.find_by(name: kingdom_attrs['name'])
-        expect(kingdom).to be_present
-
-        general_keys = (kingdom_attrs['generals'] || []).map { |g| g['key'] || g[:key] }
-        general_keys.each do |general_key|
-          general_name = general_templates[general_key.to_s]
-          expect(
-            kingdom.generals.find_by(name: general_name)
-          ).to be_present, "Expected #{kingdom.name} to have general named #{general_name}"
-        end
-      end
-
-      # Check independent generals
-      (scenario_template['independent_generals'] || []).each do |general_ref|
-        general_key = general_ref['key'] || general_ref[:key]
-        general_name = general_templates[general_key.to_s]
-        general = world.generals.find_by(name: general_name)
-        expect(general).to be_present, "Expected independent general named #{general_name} to exist"
-        expect(general.kingdom).to be_nil, "Expected independent general #{general_name} to not belong to any kingdom"
-      end
-    end
-  end
 end
--- api/spec/models/story_spec.rb
+++ api/spec/models/story_spec.rb
@@ -14,17 +14,4 @@ RSpec.describe Story, type: :model do
     expect(story.game).to be_a(Game)
     expect(story.scenario).to be_a(Scenario)
   end
-
-  Story.templates.each_key do |template_name|
-    it "generates a valid world for template '#{template_name}'", :slow do
-      game = create(:game)
-      world = game.world
-      expect { world.select_story(template_name) }.not_to raise_error
-      expect(game).to be_valid
-      expect(world).to be_valid
-      expect(world.map).to be_valid
-      expect(world.kingdoms.where(is_player_kingdom: true).count).to eq(1)
-      expect(world.player_kingdom).to be_valid
-    end
-  end
 end
